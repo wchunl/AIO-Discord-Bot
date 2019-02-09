630a36f74c92c5e59565e658237a43e0e5fae52c
Wai Chun Leung <wchunl.lss@gmail.com>
2019-02-08 18:33:13 -0800

    cleanup & polishing

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@ import json
 import requests
 import random
 import glob
+import datetime
 from difflib import SequenceMatcher
 from googletrans import Translator
 from googletrans import LANGUAGES
@@ -15,34 +16,86 @@ import asyncio
 import config as cfg
 TOKEN = cfg.TOKEN
 
-client = commands.Bot(command_prefix = 'pls ')
+client = commands.Bot(command_prefix = 'pls ', case_insensitive=True)
 client.remove_command('help')
 
 async def reply(ctx, str = None):
     await client.send_message(ctx.message.channel, "{} ".format(ctx.message.author.mention) + str)
 
+def get_channel(channels, channel_name):
+    for channel in client.get_all_channels():
+        if channel.name == channel_name:
+            return channel
+    return None
+
+def get_similar(keyword, list):
+    maxim = -1
+    result = None
+    for key in list:
+        if SequenceMatcher(None, keyword, key).ratio() > maxim:
+            maxim = SequenceMatcher(None, keyword, key).ratio()
+            result = key
+    if result == None:
+        return " "
+    else:
+        return ", did you mean ***" + result + "***?"
+
 @client.event
 async def on_ready():
     await client.change_presence(game=discord.Game(name='type "pls help" for more info'))
     print('bot is ready')
+    url = "https://api.reddit.com/user/pete7201/m/dankmemenetwork/top.json?sort=top&t=day&limit=100"
+    while True:
+        await asyncio.sleep(60*60*12) #every minute
+        chanl = get_channel(client.get_all_channels(), 'bots')
+        await client.send_message(chanl, "TODAYS DAILY MEME: ")
+        await client.send_message(chanl, embed=redditEmbed(url))
 
 @client.event
 async def on_message(message):
-    print(message.channel.name)
-    if message.author != client.user and message.channel.name == 'autimatic-text-to-speech':
-        await client.send_message(message.channel, message.content, tts=True)
-    await client.process_commands(message)
+    if message.author != client.user:
+        if  message.channel.name == 'autimatic-text-to-speech':
+            print("place holder")
+            # await client.send_message(message.channel,  .content, tts=True)
+        elif message.content.startswith("pls") or message.content.startswith("Pls"):
+            print(message.content)
+            if message.channel.name == 'bots':
+                await client.process_commands(message)
+            else:
+                await client.send_message(message.channel, "Please use the `bots` text channel.")
+
+@client.event
+async def on_command_error(error, ctx):
+    if isinstance(error, commands.CommandNotFound):
+        await reply(ctx, "Command not found, did you mean `pls " + get_similar(ctx.message.content, client.commands) + "`? For a list of commands type `pls help`.")
+    else:
+        print(error)
+        await reply(ctx, "An error occured, please try again.")
 
 @client.command(pass_context=True)
 async def help(ctx):
     author = ctx.message.author
-    
     embed = discord.Embed(
+        title = 'Commands List',
+        description = 'All commands are called with the prefix `pls`. Replace the brackets [arg] with your desired input',
         colour = discord.Colour.blue()
     )
-    embed.set_author(name='Help')
-    embed.add_field(name='.ping', value="Returns Pong!", inline=False)
+    embed.set_author(name='Dench Memer v2.0')
+    embed.add_field(name='General Commands', value="=====================", inline=False)
+    embed.add_field(name='>> `pls sound [name]`', value="Plays a soundboard, must be in a voice channel, type `pls sound list` for a list of sounds", inline=False)
+    embed.add_field(name='>> `pls translate "[text]" "[soruce]"`', value="Translate [text] to [source] language, type `pls langs` for a list of languages", inline=False)
+    embed.add_field(name='Reddit Commands', value="=====================", inline=False)
+    embed.add_field(name='>> `pls meme`', value="Sends a dank meme")
+    embed.add_field(name='>> `pls joke`', value="Sends a dank joke")
+    embed.add_field(name='>> `pls shitpost`', value="Sends a dank shitpost")
+    embed.add_field(name='>> `pls wholesome`', value="Sends a wholesome meme")
+    embed.add_field(name='>> `pls pun`', value="Sends a dank pun")
+    embed.add_field(name='>> `pls reddit [subreddit]`', value="Sends a random post from a given subreddit", inline=False)
+    embed.add_field(name='Misc Commands', value="=====================", inline=False)
+    embed.add_field(name='>> `pls echo "[text]"`', value="Repeats the message", inline=False)
+    embed.add_field(name='>> `pls ping`', value="Ping Pong command", inline=False)
     await client.send_message(author, embed=embed)
+    await reply(ctx, "Sent you a dm.")
 
 #####################################################################################
 #####                              REDDIT COMMANDS                              #####
@@ -50,8 +103,17 @@ async def help(ctx):
 # Skeleton for embedding reddit posts
 def redditEmbed(url):
     response = requests.get(url, headers = {'User-agent': 'test'}).json()
-    if response["data"]["dist"] < 100:
-        return discord.Embed(colour = discord.Colour.red(), description = "The subreddit doesn't exist/work!")
+    # print(response["data"])
+    valid = False
+    try:
+        response["data"]["dist"]
+        valid = True
+    except KeyError:
+        valid = False
+    if not valid:
+        return discord.Embed(colour = discord.Colour.red(), description = "This subreddit is private!")
+    elif response["data"]["dist"] < 100:
+        return discord.Embed(colour = discord.Colour.red(), description = "This subreddit doesn't exist/work!")
     else:
         post = response["data"]["children"][random.randint(1,100)]
         embed = discord.Embed(
@@ -85,7 +147,7 @@ async def wholesome(ctx):
     await client.send_message(ctx.message.channel, embed=redditEmbed(url))
 
 @client.command(pass_context=True)
-async def puns(ctx):
+async def pun(ctx):
     url = "https://api.reddit.com/r/puns/top/.json?sort=top&t=week&limit=100"
     await client.send_message(ctx.message.channel, embed=redditEmbed(url))
 
@@ -98,8 +160,9 @@ async def reddit(ctx, subr = None):
         url = "https://api.reddit.com/r/" + subr + "/top/.json?sort=top&t=week&limit=100"
         await client.send_message(ctx.message.channel, embed=redditEmbed(url))
 
-#----------------------------------------------------------------------------------#
-
+#####################################################################################
+#####                             GENERAL COMMANDS                              #####
+#####################################################################################
 
 
 @client.command(pass_context=True)
@@ -118,18 +181,32 @@ async def translate(ctx, text = None, target = None):
         print(translation)
         await reply(ctx, "`Language detected: " + translation.src + "` \n" + translation.origin + " ` -> ` " + translation.text)
     else:
-        await reply(ctx, "Language not found, did you mean ***" + similar + "***? For a list of available languages, type `pls help`")
+        await reply(ctx, "Language not found, did you mean ***" + similar + "***? For a list of available languages, type `pls langs`")
 
+@client.command(pass_context=True)
+async def langs(ctx):
+    langs = []
+    key = []
+    for key in LANGUAGES:
+        langs.append(LANGUAGES[key])
+    embed = discord.Embed(
+        title = "Here's a list of all available languages: ",
+        description = ", ".join(langs),
+        colour = discord.Colour.blue()
+    )
+    await client.send_message(ctx.message.author, embed=embed)
+    await reply(ctx, "Sent you a dm.")
 
 @client.command(pass_context=True)
 async def sound(ctx, file = None):
+    files = [os.path.splitext(os.path.basename(x))[0] for x in glob.glob('./assets/*.mp3')]
+    print(files)
     if file == None:
         await reply(ctx, "Please specify a sound.")
     elif file == 'list':
-        files = ", ".join(glob.glob("./assets/*.mp3"))
-        await reply(ctx, "Heres all the sounds I found: "+files.replace('./assets/', '').replace('.mp3', ''))
+        await reply(ctx, "Heres all the sounds I found: " + files)
     elif not os.path.exists('./assets/' + file + '.mp3'):
-        await reply(ctx, "The sound file `" + file + "` does not exist.")
+        await reply(ctx, "Could not find sound `" + file + "`, did you mean `" + get_similar(file,files) + "`?")
     else:
         channel = ctx.message.author.voice.voice_channel
         if channel != None:
@@ -142,6 +219,10 @@ async def sound(ctx, file = None):
             await vc.disconnect()
         else:
             await reply(ctx, "Join a voice channel first.")
+
+#####################################################################################
+#####                               MISC COMMANDS                               #####
+#####################################################################################
 
 @client.command()
 async def ping():
